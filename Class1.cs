85a9f20e4065c10aef4ffbb4f7e97892fd373075
srinivasvanka009 <44634964+srinivasvanka009@users.noreply.github.com>
2020-07-18 19:48:02 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestLib
+{
+    public class Class1
+    {
+        public void nothing()
+        {
+
+        }
+        public void nothing1()
+        {
+
+        }
+    }
+}
